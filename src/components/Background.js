c3857cc94e80564d419b75b4581a35b74a9e0f3c
David_Yu <yuyang545262477@gmail.com>
2016-06-04 11:58:10 +0800

    1.Change PIcs. 2. write refs && module

---
+++ src/components/Background.js
@@ -0,0 +1,16 @@
+require('../styles/App.css');
+
+import React from "react";
+
+
+class Background extends React.Component {
+    render() {
+        return (
+            <h2>hello Background</h2>
+        );
+    }
+}
+
+Background.defaultProps = {};
+
+export default Background;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,9 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+// require('styles/App.css');
 require('styles/custom.scss');
 
 import React from "react";
+import ReactDOM from "react-dom";
 
 //引入JSON文件
 var ImageDatas = require('../data/imageDatas.json');
@@ -17,21 +18,116 @@ ImageDatas = (function (imageDatasArr) {
     return imageDatasArr;
 })(ImageDatas);
 
-class AppComponent extends React.Component {
+
+/*  图片信息 */
+class ImgFigure extends React.Component {
+    // alert('ImgFigure'+ Constant.centerPos);
+    
     render() {
         return (
-            <section className="stage">
+            <figure className="img-figure">
+                <img src={this.props.data.imageURL}
+                     alt={this.props.data.title}/>
+                <figcaption>
+                    <h2 className="img-title">{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        )
+    }
+    
+}
+
+
+/* 组建背景 */
+class GalleryByReactApp extends React.Component {
+    
+    constructor() {
+        super();
+        //初始化位置,常量.
+        this.Constant = {
+            centerPos: {
+                left: 0,
+                right: 0
+            },
+            hPosRange: { //水平方向的取值范围
+                leftSecX: [0, 0],
+                rightSecX: [0, 0],
+                y: [0, 0]
+            },
+            vPosRange: { //垂直方向
+                x: [0, 0],
+                topY: [0, 0]
+            }
+        };
+        
+    }
+    
+    rearrage(centerIndex) {
+        
+    }
+    
+    
+    componentDidMount() {
+        //     拿到舞台的大小
+        var stageDOM = ReactDOM.findDOMNode(this.refs.stage),
+            stageW = stageDOM.scrollWidth,
+            stageH = stageDOM.scrollHeight,
+            halfStageW = Math.ceil(stageW / 2),
+            halfStageH = Math.ceil(stageH / 2);
+        
+        //    拿到每一个imageFigure的大小
+        var imgFigureDOM = ReactDOM.findDOMNode(this.refs.imgFigure0),
+            imgW = imgFigureDOM.scrollWidth,
+            imgH = imgFigureDOM.scrollHeight,
+            halfImgW = Math.ceil(imgW / 2),
+            halfImgH = Math.ceil(imgH / 2);
+        
+        
+        //    计算中心图片的位置点
+        
+        this.Constant.centerPos = {
+            left: halfStageW - halfImgW,
+            top: halfStageH - halfImgH
+        };
+        //    计算图片左侧右侧。
+        this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+        this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+        this.Constant.hPosRange.rightSecX[0] = halfStageW + halfImgW;
+        this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+        this.Constant.hPosRange.y[0] = -halfImgH;
+        this.Constant.hPosRange.y[1] = stageH - halfImgH;
+        
+        
+        //    计算图片上侧的排布位置。
+        this.Constant.vPosRange.x[0] = halfStageW - imgW;
+        this.Constant.vPosRange.x[1] = halfStageW;
+        this.Constant.vPosRange.topY[0] = -imgH;
+        this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+    }
+    
+    
+    render() {
+        var controllerUnits = [],
+            imgFigures = [];
+        
+        ImageDatas.forEach(function (value, index) {
+            imgFigures.push(<ImgFigure data={value} ref={'imgFigure'+index} key={index}/>);
+        }.bind(this));
+        return (
+            <section className="stage" ref="stage">
                 <section className="img-sec">
-                    Hello world
+                    {imgFigures}
                 </section>
                 <nav className="controller-nav">
-                    I love constructor
+                    {controllerUnits}
                 </nav>
             </section>
         );
     }
+    
+    
 }
 
-AppComponent.defaultProps = {};
+GalleryByReactApp.defaultProps = {};
 
-export default AppComponent;
+export default GalleryByReactApp;
